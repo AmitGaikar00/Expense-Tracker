5c6eb9a745e8e1267a2e1190bec59e4fb85200cc
AmitGaikar00 <amitgaikar6025@gmail.com>
2023-10-21 13:08:24 +0530

    node backend starting template

---
+++ Backend/middleware/errorHandler.js
@@ -0,0 +1,9 @@
+export const errorResponserHandler = (err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+
+  res.status(errorStatus).json({
+    message: errorMessage,
+    stack: err.stack,
+  });
+};
